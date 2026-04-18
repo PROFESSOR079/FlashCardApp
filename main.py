e7abb00694c918b848388608c5151e46873d2ca4
Gugalomi=geo <iva290386@gmail.com>
2026-04-18 21:07:37 +0400

    add random word generation logic using pandas and random

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 from tkinter import *
+import pandas
+import random
 
 BACKGROUND_COLOR = "#B1DDC6"
+data = pandas.read_csv("data/french_words.csv")
+to_learn = data.to_dict(orient="records")
+
+def next_card():
+    current_card = random.choice(to_learn)
+    canvas.itemconfig(card_title, text="French")
+    canvas.itemconfig(card_word, text=current_card["French"])
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -11,19 +20,21 @@ window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 canvas = Canvas(width=800, height=526)
 card_front_img = PhotoImage(file="images/card_front.png")
 canvas.create_image(400, 263, image=card_front_img)
-canvas.create_text(400, 150, text="Title", font=("Ariel", 40, "italic"))
-canvas.create_text(400, 263, text="word", font=("Ariel", 60, "bold"))
+card_title = canvas.create_text(400, 150, text="", font=("Ariel", 40, "italic"))
+card_word = canvas.create_text(400, 263, text="", font=("Ariel", 60, "bold"))
 
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 canvas.grid(row=0, column=0, columnspan=2)
 
 # BUTTONS
 unknown_btn_img = PhotoImage(file="images/wrong.png")
-unknown_btn = Button(image=unknown_btn_img, bd=0, highlightthickness=0)
+unknown_btn = Button(image=unknown_btn_img, bd=0, highlightthickness=0, command=next_card)
 unknown_btn.grid(row=1, column=0)
 
 check_btn_img = PhotoImage(file="images/right.png")
-check_btn = Button(image=check_btn_img, bd=0, highlightthickness=0)
+check_btn = Button(image=check_btn_img, bd=0, highlightthickness=0, command=next_card)
 check_btn.grid(row=1, column=1)
+
+next_card()
 
 window.mainloop()
